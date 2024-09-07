df69a64249606d4c2fb1872ae45c0928bef4ca47
Mike Khaw <mikekhawnow@gmail.com>
2024-09-07 08:49:02 -0700

    done!

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import OpenAI from 'openai';
+import { NextResponse } from 'next/server';
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages,
+    });
+
+    const stream = OpenAIStream(response)
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({ error: 'An error occurred during your request.' }, { status: 500 });
+  }
+}
